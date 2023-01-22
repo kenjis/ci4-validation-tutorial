f20fc3bc520adf663d936525e98994e9f9c8d5ee
kenjis <kenji.uui@gmail.com>
2023-01-22 09:21:47 +0900

    feat: show validation errors with the input fields

--- app/Views/form2.php
+++ app/Views/form2.php
@@ -10,15 +10,24 @@
 
         <h5>Username</h5>
         <input type="text" name="username" value="<?= set_value('username') ?>" size="50">
+        <?php
+        $errors = validation_errors();
+        if (isset($errors['username'])) {
+            echo validation_show_error('username');
+        }
+        ?>
 
         <h5>Password</h5>
         <input type="text" name="password" value="<?= set_value('password') ?>" size="50">
+        <?= validation_show_error('password') ?>
 
         <h5>Password Confirm</h5>
         <input type="text" name="passconf" value="<?= set_value('passconf') ?>" size="50">
+        <?= validation_show_error('passconf') ?>
 
         <h5>Email Address</h5>
         <input type="text" name="email" value="<?= set_value('email') ?>" size="50">
+        <?= validation_show_error('email') ?>
 
         <div><input type="submit" value="Submit"></div>
 
