0db0bda900d2f28cdff9ef21d7c9dcaad6705927
kenjis <kenji.uui@gmail.com>
2023-08-15 06:26:23 +0900

    fix: XSS

--- app/Views/success.php
+++ app/Views/success.php
@@ -7,10 +7,10 @@
     <h3>Your form was successfully submitted!</h3>
 
     <p>
-    Username: <?= $username ?><br>
-    Password: <?= $password ?><br>
-    Email: <?= $email ?><br>
-    Fruit: <?= implode(', ', $fruit)?><br>
+    Username: <?= esc($username) ?><br>
+    Password: <?= esc($password) ?><br>
+    Email: <?= esc($email) ?><br>
+    Fruit: <?= esc(implode(', ', $fruit)) ?><br>
     </p>
 
     <p><?= anchor('form1', 'Go to form1') ?></p>
